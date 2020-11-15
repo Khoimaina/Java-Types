55fc725b237ed12638ab2df2a30045917065ef0d
Khoimaina <khoibig@gmail.com>
2020-11-14 23:13:52 -0500

    my first java commit

---
+++ src/demo.java
@@ -0,0 +1,15 @@
+
+public class demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    String firstname = "Teresa";
+        System.out.println(firstname);
+        String _lastname;
+        System.out.println(_lastname);
+	}
+
+}
